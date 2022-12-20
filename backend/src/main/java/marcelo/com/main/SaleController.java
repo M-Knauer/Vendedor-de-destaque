ee387c82ab174321845ad639313a71b15ec1eaab
unknown <marcelocrisknauer@gmail.com>
2022-12-19 23:53:34 -0300

    Date select

--- backend/src/main/java/marcelo/com/main/SaleController.java
+++ backend/src/main/java/marcelo/com/main/SaleController.java
@@ -1,11 +1,12 @@
 package marcelo.com.main;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +22,11 @@ public class SaleController {
 	
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.FOUND)
-	public List<Sale> findSales() {
-		return ss.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			Pageable pageable) {
+		return ss.findSales(minDate, maxDate, pageable);
 	}
 	
 	
--- backend/src/main/java/marcelo/com/main/services/SaleService.java
+++ backend/src/main/java/marcelo/com/main/services/SaleService.java
@@ -1,8 +1,12 @@
 package marcelo.com.main.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,12 +21,14 @@ public class SaleService {
 	SaleRepository sr;
 	
 	@Transactional(readOnly = true)
-	public List<Sale> findSales() {
-		return sr.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.isEmpty() ? today.minusYears(1) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.isEmpty() ? today : LocalDate.parse(maxDate);
+		
+		return sr.findSales(min, max, pageable);
 	}
 	
-	public Sale findSale(Long id) {
-		return sr.findById(id).get();
-		
-	}
 }
